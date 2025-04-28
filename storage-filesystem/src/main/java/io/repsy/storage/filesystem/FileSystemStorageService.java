186ad2c5b9aa95d3f8f77778b1744ef74c8404d0
Umut Tölek <umuttolekk@gmail.com>
2025-04-28 19:27:12 +0300

    Local file storage implemented

--- storage-filesystem/src/main/java/io/repsy/storage/filesystem/FileSystemStorageService.java
+++ storage-filesystem/src/main/java/io/repsy/storage/filesystem/FileSystemStorageService.java
@@ -2,16 +2,38 @@ package io.repsy.storage.filesystem;
 
 import io.repsy.storage.StorageService;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.*;
 
 public class FileSystemStorageService implements StorageService {
+    private final Path rootPath;
+
+    public FileSystemStorageService(String rootPath) {
+        this.rootPath = Paths.get(rootPath);
+    }
+
     @Override
     public void save(String path, InputStream content) {
-
+        try {
+            Path fullPath = rootPath.resolve(path);
+            Files.createDirectories(fullPath.getParent());
+            Files.copy(content, fullPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("FileSystemStorageService.save failed for path: " + path, e);
+        }
     }
 
     @Override
     public InputStream load(String path) {
-        return null;
+        try {
+            Path fullPath = rootPath.resolve(path);
+            if (!Files.exists(fullPath)) {
+                throw new RuntimeException("File not found on filesystem: " + fullPath);
+            }
+            return Files.newInputStream(fullPath, StandardOpenOption.READ);
+        } catch (IOException e) {
+            throw new RuntimeException("FileSystemStorageService.load failed for path: " + path, e);
+        }
     }
 }
