e3c76df27ca20daa8ff3d117111617ad203f0139
Umut Tölek <umuttolekk@gmail.com>
2025-04-28 19:27:26 +0300

    Minio storage implemented

--- storage-object/src/main/java/io/repsy/storage/objectstorage/ObjectStorageService.java
+++ storage-object/src/main/java/io/repsy/storage/objectstorage/ObjectStorageService.java
@@ -1,17 +1,50 @@
 package io.repsy.storage.objectstorage;
 
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import io.repsy.storage.StorageService;
 
 import java.io.InputStream;
 
 public class ObjectStorageService implements StorageService {
+    private final MinioClient minioClient;
+    private final String bucketName;
+
+    public ObjectStorageService(String endpoint, String accessKey,String secretKey, String bucketName) {
+        this.minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        this.bucketName = bucketName;
+    }
+
     @Override
     public void save(String path, InputStream content) {
-
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(path)
+                            .stream(content, -1, 10 * 1024 * 1024)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("ObjectStorageService.save failed for path: " + path, e);
+        }
     }
 
     @Override
     public InputStream load(String path) {
-        return null;
+        try {
+            return minioClient.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(path)
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("ObjectStorageService.load failed for path: " + path, e);
+        }
     }
 }
