cff0ba2b4b38899bb8aa9659f267ef3b2570df29
Umut Tölek <umuttolekk@gmail.com>
2025-04-28 19:26:33 +0300

    PackageRepository created

--- repsy-api/src/main/java/io/repsy/repsy_api/repository/PackageRepository.java
+++ repsy-api/src/main/java/io/repsy/repsy_api/repository/PackageRepository.java
@@ -1,4 +1,10 @@
 package io.repsy.repsy_api.repository;
 
-public class PackageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import io.repsy.repsy_api.entity.Package;
+import java.util.Optional;
+
+public interface PackageRepository extends JpaRepository<Package, Long> {
+    Optional<Package> findByNameAndVersion(String name, String version);
 }
+
